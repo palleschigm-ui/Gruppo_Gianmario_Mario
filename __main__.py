7858e7c4b2539bc3366abde8c195a038dc24a6b5
palleschigm-ui <palleschi.gm@gmail.com>
2025-11-18 17:10:11 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -21,3 +21,18 @@ matrice=GestoreMatrice([[1,2,3],[3,3,3],[1,2,3],[1,2,5,6]])
 matrice.stampa_matrice()
 valid=matrice.valida_matrice()
 print(valid)
+
+class ElaboratoreStatistico(GestoreMatrice):
+    def __init__(self, matrice):
+        GestoreMatrice.__init__(self,matrice)
+    def trova_massimo(self):
+        return max(max(self.matrice))
+    def media_riga(self,indice_riga):
+        n=len(self.matrice[indice_riga])
+        return sum(self.matrice[indice_riga])/n
+
+matrice=ElaboratoreStatistico([[1,2,3],[3,3,3],[1,2,3],[1,2,5,6]])        
+print(matrice.trova_massimo())
+print(matrice.media_riga(1))
+
+
